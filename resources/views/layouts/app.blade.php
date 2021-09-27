8214ad9d1d7935169e998f6fb63df4bc3c2bc46d
azarashiidesu <88758080+azarashiidesu@users.noreply.github.com>
2021-09-27 18:09:16 +0800

    added jquery cdn

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,6 +11,7 @@
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
+    
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
@@ -98,6 +99,7 @@
         </main>
 
         <div>
+            <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
             @yield('script')
         </div>
         
