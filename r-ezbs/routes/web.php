20362d5e49f5da7ba6c9e9c3caf1323b71218483
azarashiidesu <88758080+azarashiidesu@users.noreply.github.com>
2021-09-27 14:46:29 +0800

    working room index

--- r-ezbs/routes/web.php
+++ r-ezbs/routes/web.php
@@ -1,17 +1,7 @@
 <?php
 
+use App\Http\Controllers\RoomController;
 use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 
 Route::get('/', function () {
     return view('welcome');
@@ -20,3 +10,4 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::resource('room', RoomController::class);
