e1b7396175f4dc871f179c8d7c771c7fa9dfb788
OmAnignat <65340148+OmAnignat@users.noreply.github.com>
2021-10-01 19:38:20 +0800

    sweetalert added in users

--- public/admin/js/user.js
+++ public/admin/js/user.js
@@ -1,3 +1,12 @@
+const swalWithBootstrapButtons = Swal.mixin({
+  customClass: {
+      confirmButton: 'btn btn-success mx-2',
+      cancelButton: 'btn btn-danger mx-2'
+      
+  },
+  buttonsStyling: false
+});
+
 $(function () {
     //console.log("user loaded");
     getTable();
@@ -16,6 +25,17 @@ $(function () {
   // Delete Ajax
   $(document).on('submit', '.deleteUser', function (event) {
     event.preventDefault();
+    swalWithBootstrapButtons.fire({
+      title: 'Are you sure?',
+      text: "This User will be delete from the database!",
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonText: 'Yes, Delete it!',
+      cancelButtonText: 'No, Cancel!',
+      reverseButtons: true
+      
+      }).then((result) => {
+      if (result.isConfirmed) {
     console.log("Checking Delete Form");
     const form = this;
 
@@ -31,19 +51,40 @@ $(function () {
         console.log(response);
             getTable(1);
         }
+
       });
+    } else if (
+      result.dismiss === Swal.DismissReason.cancel // click ayaw
+      ) {
+      swalWithBootstrapButtons.fire(
+          'Cancelled',
+          'Lets pretend that never happend >:)',
+          'error'
+      )
+  }
+  })
   });
 
   // Add Ajax
   $('#addUserForm').on('submit', function (event) {
     event.preventDefault();
-    console.log("Checking Add Form");
-    const form = this;
+    swalWithBootstrapButtons.fire({
+      title: 'Are you sure?',
+      text: "This User will be added from the database!",
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonText: 'Yes, Add it!',
+      cancelButtonText: 'No, Cancel!',
+      reverseButtons: true
+      
+      }).then((result) => {
+      if (result.isConfirmed) {
+      console.log("Checking Add Form");
+      const form = this;
   
     $.ajax({
           url: $(form).attr('action'),
           method: $(form).attr('method'),
-          
           dataType: 'JSON',
           data: new FormData(form),
           processData: false,
@@ -58,4 +99,14 @@ $(function () {
           console.log(response);
         }
       });
+    } else if (
+      result.dismiss === Swal.DismissReason.cancel // click ayaw
+      ) {
+      swalWithBootstrapButtons.fire(
+          'Cancelled',
+          'Lets pretend that never happend >:)',
+          'error'
+      )
+  }
+  })
   });
