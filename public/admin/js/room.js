dc3966fe4443a8dff09406ebe14df219d680197b
Arellano Hanz Mark <69075813+bskiller211@users.noreply.github.com>
2021-10-05 11:07:00 +0800

    Working Room CRUD

--- public/admin/js/room.js
+++ public/admin/js/room.js
@@ -1,5 +1,4 @@
 $(function () {
-    console.log("room loaded");
     getTable();
   });
   
@@ -13,6 +12,25 @@ $(function () {
         },
     });
   }
+
+  // Display Edit Form
+  $(document).on('click', '#editBtn', function(event){
+    const id = ($(this).attr('dataId'));
+    console.log(id);
+    const form = this;
+      $.ajax({
+        method: 'GET',
+        url: 'room/'+id+'/edit/',
+        processData: false,
+        contentType: false,
+        success:function(response){
+        console.log(response);
+          $('#editRoomForm').html(response);
+        },
+      });
+  });
+  
+
   // Delete Ajax
   $(document).on('submit', '.deleteRoom', function (event) {
     event.preventDefault();
@@ -59,3 +77,31 @@ $(function () {
         }
       });
   });
+
+  
+
+  // Get Data Ajax
+  // $('#editRoomButton').on('click', function (event) {
+  //   event.preventDefault();
+  //   console.log("Checking Edit Btn");
+  //   //const form = this;
+    
+  //   $.ajax({
+  //         url: $(form).attr('action'),
+  //         method: $(form).attr('method'),
+  //         type: 'POST',
+  //         dataType: 'JSON',
+  //         data: new FormData(form),
+  //         processData: false,
+  //         contentType: false,
+  //       success: function (response){
+  //         $('#addRoomModal').modal('hide');
+  //         $('body').removeClass('modal-open');
+  //         $('.modal-backdrop').remove();
+  //         console.log(response);
+  //         getTable();
+  //       },error: function(response){
+  //         console.log(response);
+  //       }
+  //     });
+  // });
--- resources/views/room.blade.php
+++ resources/views/room.blade.php
@@ -12,7 +12,7 @@
             </div>
     </div>
     
-  <!-- Modal -->
+  <!-- Add Modal -->
   <div class="modal fade" id="addRoomModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
     <div class="modal-dialog">
       <div class="modal-content">
@@ -48,6 +48,23 @@
                 <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
                 <button type="submit" class="btn btn-primary">Save changes</button>
             </form>
+        </div>
+      </div>
+    </div>
+  </div>
+  
+  <!-- Update Modal -->
+  <div class="modal fade" id="editRoomModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="exampleModalLabel">Edit Room</h5>
+          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        </div>
+        <div class="modal-body">
+          <div id="editRoomForm">
+            {{-- Table Generated from admin.js --}}
+          </div>
         </div>
       </div>
     </div>
