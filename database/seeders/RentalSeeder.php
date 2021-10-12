0e99aeaf42d555f3cb616c3c127504a9f76543d3
Niblegen <judaya93@gmail.com>
2021-10-12 11:54:12 +0800

    fixed timestamp

--- database/seeders/RentalSeeder.php
+++ database/seeders/RentalSeeder.php
@@ -4,6 +4,9 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 
+// for created_at and updated_at column value
+use Carbon\Carbon;
+
 // For sql query
 use Illuminate\Support\Facades\DB;
 
@@ -21,30 +24,40 @@ class RentalSeeder extends Seeder
                 'price' => '800.00',                            //per hour
                 'description' => 'Enjoy the thrilling and adrenaline pumping trails that we have for you.',
                 'picture' => 'RentalPicture',
+                'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+                'updated_at' => Carbon::now()->format('Y-m-d H:i:s'),
             ],
             [
                 'name' => 'ATV With Backride',
                 'price' => '1200.00',                           //per hour
                 'description' => 'Ride with a partner over the sand and dirt.',
                 'picture' => 'RentalPicture',
+                'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+                'updated_at' => Carbon::now()->format('Y-m-d H:i:s'),
             ],
             [
                 'name' => 'Tent',
                 'price' => '550.00',
                 'description' => 'Experience the great outdoors and relax as you hear the waves roll in front of you.',
                 'picture' => 'RentalPicture',
+                'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+                'updated_at' => Carbon::now()->format('Y-m-d H:i:s'),
             ],
             [
                 'name' => 'Volleyball',
                 'price' => '250.00',                           //per 2 hours
                 'description' => 'Have a fun and exciting match with your friends.',
                 'picture' => 'RentalPicture',
+                'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+                'updated_at' => Carbon::now()->format('Y-m-d H:i:s'),
             ],
             [
                 'name' => 'Surf',
                 'price' => '1000.00',                          //board and instructor
                 'description' => 'Ride the incredible waves of Liwliwa.',
                 'picture' => 'RentalPicture',
+                'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+                'updated_at' => Carbon::now()->format('Y-m-d H:i:s'),
             ],
             
         ];
