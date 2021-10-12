b8fa91b15048227f4a5bc6beaaef7ded86320921
Niblegen <judaya93@gmail.com>
2021-10-12 15:22:54 +0800

    relationship transaction and user

--- app/Models/User.php
+++ app/Models/User.php
@@ -45,4 +45,9 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function transactions()
+    {
+        return $this->hasMany('App\Transaction');
+    }
 }
