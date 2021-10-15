1369a24eec1bf14482fc025796d02a884acb1ff0
Niblegen <judaya93@gmail.com>
2021-10-15 21:41:56 +0800

    home test for joms

--- resources/views/components/customerComponents/home.blade.php
+++ resources/views/components/customerComponents/home.blade.php
@@ -1,248 +1,183 @@
 @extends('layouts.app')
 
 @section('script')
-
-
-{{-- <link href="https://cdn.jsdelivr.net/npm/fullcalendar@5.9.0/main.min.css" rel="stylesheet">
-<script src="https://cdn.jsdelivr.net/npm/fullcalendar@5.9.0/main.min.js"></script> --}}
-
-<link rel="stylesheet"
-    href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.9.0/css/bootstrap-datepicker.min.css"
-    integrity="sha512-mSYUmp1HYZDFaVKK//63EcZq4iFWFjxSL+Z3T/aCt4IO9Cejm03q3NKKYN6pFQzY0SBOr8h+eCIAZHPXcpZaNw=="
-    crossorigin="anonymous" referrerpolicy="no-referrer" />
-<script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.9.0/js/bootstrap-datepicker.min.js"
-    integrity="sha512-T/tUfKSV1bihCnd+MxKD0Hm1uBBroVYBOYSk1knyvQ9VyZJpc/ALb4P0r6ubwVPSGB2GvjeoMAJJImBG12TiaQ=="
-    crossorigin="anonymous" referrerpolicy="no-referrer"></script>
-<script>
-    $(function () {
-        // $('#datepicker').datepicker();
-
-        var nowDate = new Date();
-        var today = new Date(nowDate.getFullYear(), nowDate.getMonth(),
-            nowDate.getDate(), 0, 0, 0, 0);
-
-        $('.input-daterange input').each(function () {
-            $(this).datepicker({
-                todayHighlight: true,
-                // startDate: '12-12-2012',
-                startDate: today,
-                beforeShowDay: unavailable,
-            });
-        });
-    })
-
-    var unavailableDates = ["13-10-2021","14-8-2015"];
-
-function unavailable(date) {
-    
-  dmy = date.getFullYear() + "-" + (date.getMonth()+1) + "-" +date.getDate();
-  var qwe = ["13-10-2021","14-8-2015"];
-    if ($.inArray(qwe, unavailableDates) < 0) {
-        return [true,"","Book Now"];
-    } else {
-        return [false,"","Booked Out"];
-    }
-}
-
-    // // var disabledDates = ["2021-10-12", "2021-10-13", "2021-10-15"]
-    // var disabledDates = ["23-10-2021", "2021-10-13", "2021-10-15"]
-
-    // function unavailable(date) {
-    // // dmy = date.getFullYear() + "-" + (date.getMonth()+1) + "-" +date.getDate();
-    // dmy = date.getDate() + "-" + (date.getMonth()+1) + "-" +date.getFullYear();
-    // if ($.inArray(dmy, disabledDates) < 0) {
-    //     return [true,"","Book Now"];
-    // } else {
-    //     return [false,"","Booked Out"];
-    // }
-
-</script>
+    <script src="{{ asset('js/customer/customer.js') }}"></script>
 @endsection
 
+<style>
+    .myBorder {
+        border: 2px solid red;
+    }
+
+    #test {
+
+        background: url('{{ URL::asset('/img/welcome_page/welcomecoverphoto.png') }}') no-repeat center center fixed;
+        /* background: url('https://source.unsplash.com/twukN12EN7c/1920x1080') no-repeat center center fixed; */
+        -webkit-background-size: cover;
+        -moz-background-size: cover;
+        background-size: cover;
+        -o-background-size: cover;
+    }
+
+</style>
+
 @section('content')
-
-<!-- Modal -->
-<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-    <div class="modal-dialog modal-xl">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h5 class="modal-title" id="exampleModalLabel">Room</h5>
-                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-            </div>
-            <div class="modal-body">
+<div id="test" class="">
+    <div class="row justify-content-center p-5 m-0">
+        <div class="border border-white rounded-2 border-3 row bg-primary mx-2 w-100 ">
+            <form class="text-white">
                 <div class="row">
-                    <div class="col-md-4">
-                        <img src="{{ URL::asset('img\welcome_page\hut.jpg'); }}" alt=""
-                            style="width: 100%; border-radius: 5%;">
+                    <div class="col-12 col-md-7">
+                        <div class="form-group col-12">
+                            <label for="title" class="col-md-5 col-form-label">Title</label>
+                            <input id="title" type="text" class="m-0 form-control" name="title" autocomplete="title"
+                                placeholder="Title..." autofocus>
+
+                            <span class="invalid-feedback fw-bold error-text title_error"></span>
+                        </div>
+                        <div class="row">
+                            <div class="form-group col-12 col-md-6">
+                                <label for="start" class="col-md-5 col-form-label">Check In</label>
+                                <input id="start" type="date" class="m-0 form-control" name="start"
+                                    placeholder="dd/mm/yy">
+
+                                <span class="invalid-feedback fw-bold error-text start_error"></span>
+                            </div>
+                            <div class="form-group col-12 col-md-6">
+                                <label for="end" class="col-md-5 col-form-label">Check Out</label>
+                                <input id="end" type="date" class="m-0 form-control" name="end" placeholder="dd/mm/yy">
+
+                                <span class="invalid-feedback fw-bold error-text end_error"></span>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="form-group col-12 col-md-6">
+                                <label for="adult" class="col-md-5 col-form-label">Adult (18+)</label>
+                                <input id="adult" type="number" class="m-0 form-control" name="adult" placeholder="1"
+                                    value="1">
+
+                                <span class="invalid-feedback fw-bold error-text adult_error"></span>
+                            </div>
+                            <div class="form-group col-12 col-md-6">
+                                <label for="children" class="col-md-5 col-form-label">Children (0-17)</label>
+                                <input id="children" type="text" class="m-0 form-control" name="children"
+                                    placeholder="0" value="0">
+
+                                <span class="invalid-feedback fw-bold error-text children_error"></span>
+                            </div>
+                        </div>
+                        <div class="form-group col-12">
+                            <label for="description" class="col-md-5 col-form-label">Description</label>
+                            <textarea id="description" placeholder="Description (max 250 word)" class="m-0 form-control"
+                                style="height: 120px" maxlength="250" name="description"></textarea>
+
+                            <span class="invalid-feedback fw-bold error-text description_error"></span>
+                        </div>
                     </div>
-                    <div class="col-md-8">
-                        <div class="row">
-                            <p>Description</p>
-                            <p>Details</p>
-                        </div>
-                        <div class="row">
-                            <div class="input-group input-daterange">
-                                <input type="text" class="form-control">
-                                <div class="input-group-addon mx-2">to</div>
-                                <input type="text" class="form-control">
-                            </div>
-                            {{-- <div class="input-group date" id="datepicker">
-                                <input type="text" class="form-control">
-                                <span class="input-group-text bg-white">
 
-                                </span>
-                            </div>
-                            <input class="date" type="date"> --}}
-                            {{-- <div class="input-append date" id="dp3" data-date="12-02-2012" data-date-format="dd-mm-yyyy"> 
-                                <input class="span2" size="16" type="text" value="12-02-2012"> 
-                                    <span class="add-on"><i class="icon-th"></i></span>
-                              </div> --}}
-                        </div>
+                    <div class="col-12 col-md-5">
+                        <div id="menu">
+                            <div class="panel list-group">
+                                <a href="#" class="col-md-12" data-bs-toggle="collapse"
+                                    data-bs-target="#roomsContainer" data-bs-parent="#menu" aria-expanded="true">
+                                    <label for="description" class="col-md-5 col-form-label text-white">Select
+                                        Rooms</label>
+                                    <i class="bi bi-house-fill"></i>
+                                    <span class="glyphicon glyphicon-envelope pull-right"></span>
+                                </a>
+                                <div id="roomsContainer" class="sublinks collapse show">
+                                    <a href="#" class="list-group-item small"><span
+                                            class="glyphicon glyphicon-chevron-right"></span>
+                                        inbox</a>
+                                    <a class="list-group-item small "><span
+                                            class="glyphicon glyphicon-chevron-right"></span> sent</a>
+                                </div>
 
+                                <a href="#" class="col-md-12" data-bs-toggle="collapse"
+                                    data-bs-target="#sl" data-bs-parent="#menu">
+                                    <label for="description" class="col-md-5 col-form-label text-white">Select
+                                        Rentals</label>
+                                    <i class="bi bi-house-fill"></i>
+                                    <span class="glyphicon glyphicon-envelope pull-right"></span>
+                                </a>
+                                <div id="sl" class="sublinks collapse">
+                                    <a class="list-group-item small"><span
+                                            class="glyphicon glyphicon-chevron-right"></span> saved tasks</a>
+                                    <a class="list-group-item small"><span
+                                            class="glyphicon glyphicon-chevron-right"></span> add new
+                                        task</a>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
-            </div>
-            <div class="modal-footer">
-                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                <button type="button" class="btn btn-primary">Rent</button>
-            </div>
+            </form>
         </div>
     </div>
 </div>
 
-
-<div class="container">
-    <h1>Rent</h1>
-    <div class="row">
-        <div class="col-md-8">
-
-            <button type="button" class="btn btn-secondary" style="width: 6rem">Rooms</button>
-            <button type="button" class="btn btn-secondary" style="width: 6rem">Rentals</button>
-            <button type="button" class="btn btn-secondary" style="width: 6rem">Other</button>
-
-            <div style="margin-top: 1rem">
-
-              <div style="margin-top: 1rem; margin-bottom: 1rem">
-                  <div style="border-bottom: 1px solid gray;"></div>
-              </div>
-              <div data-bs-toggle="modal" data-bs-target="#exampleModal" style="cursor: pointer;">
-                  <div class="row">
-                      <div class="col-md-3">
-                          <img src="{{ URL::asset('img\kubo_fan_room\kubo1.jpg'); }}" alt="" style="width: 100%; border-radius: 10%;">
-                      </div>
-                      <div class="col-md-9">
-                          <p>Kubo Fan Room</p>
-                          <p>Description</p>
-                      </div>
-                  </div>
-              </div>
-            
+{{-- <div class="bg-image" style="background-image: url('https://mdbootstrap.com/img/new/standard/city/041.jpg'); height: 400px;">
+    </div> --}}
+{{-- <div class="border border-primary rounded-2 border-3 row">
+        <div class="col-md-7">
+            <div class="myBorder">
+                <h1>booking details</h1>
             </div>
-
-            <div style="margin-top: 1rem">
-
-              <div style="margin-top: 1rem; margin-bottom: 1rem">
-                  <div style="border-bottom: 1px solid gray;"></div>
-              </div>
-              <div data-bs-toggle="modal" data-bs-target="#exampleModal" style="cursor: pointer;">
-                  <div class="row">
-                      <div class="col-md-3">
-                          <img src="{{ URL::asset('img\superior_room\superior1.jpg'); }}" alt="" style="width: 100%; border-radius: 10%;">
-                      </div>
-                      <div class="col-md-9">
-                          <p>Superior Room</p>
-                          <p>Description</p>
-                      </div>
-                  </div>
-              </div>
-              
-            </div>
-
-            <div style="margin-top: 1rem">
-
-              <div style="margin-top: 1rem; margin-bottom: 1rem">
-                  <div style="border-bottom: 1px solid gray;"></div>
-              </div>
-              <div data-bs-toggle="modal" data-bs-target="#exampleModal" style="cursor: pointer;">
-                  <div class="row">
-                      <div class="col-md-3">
-                          <img src="{{ URL::asset('img\villa_room\villa1.jpg'); }}" alt="" style="width: 100%; border-radius: 10%;">
-                      </div>
-                      <div class="col-md-9">
-                          <p>Villa</p>
-                          <p>Description</p>
-                      </div>
-                  </div>
-              </div>
-            
-            </div>
-
-            <div style="margin-top: 1rem">
-
-              <div style="margin-top: 1rem; margin-bottom: 1rem">
-                  <div style="border-bottom: 1px solid gray;"></div>
-              </div>
-              <div data-bs-toggle="modal" data-bs-target="#exampleModal" style="cursor: pointer;">
-                  <div class="row">
-                      <div class="col-md-3">
-                          <img src="{{ URL::asset('img\couple_bhut\couple1.jpg'); }}" alt="" style="width: 100%; border-radius: 10%;">
-                      </div>
-                      <div class="col-md-9">
-                          <p>Couple Beach Hut</p>
-                          <p>Description</p>
-                      </div>
-                  </div>
-              </div>
-            
-            </div>
-            <div style="margin-top: 1rem">
-
-              <div style="margin-top: 1rem; margin-bottom: 1rem">
-                  <div style="border-bottom: 1px solid gray;"></div>
-              </div>
-              <div data-bs-toggle="modal" data-bs-target="#exampleModal" style="cursor: pointer;">
-                  <div class="row">
-                      <div class="col-md-3">
-                          <img src="{{ URL::asset('img\regular_bhut\regular1.jpg'); }}" alt="" style="width: 100%; border-radius: 10%;">
-                      </div>
-                      <div class="col-md-9">
-                          <p>Regular Beach Hut</p>
-                          <p>Description</p>
-                      </div>
-                  </div>
-              </div>
-            
-            </div>
-            <div style="margin-top: 1rem">
-
-              <div style="margin-top: 1rem; margin-bottom: 1rem">
-                  <div style="border-bottom: 1px solid gray;"></div>
-              </div>
-              <div data-bs-toggle="modal" data-bs-target="#exampleModal" style="cursor: pointer;">
-                  <div class="row">
-                      <div class="col-md-3">
-                          <img src="{{ URL::asset('img\barkada_bhut\barkada1.jpg'); }}" alt="" style="width: 100%; border-radius: 10%;">
-                      </div>
-                      <div class="col-md-9">
-                          <p>Barkada Beach Hut</p>
-                          <p>Description</p>
-                      </div>
-                  </div>
-              </div>
-            
-            </div>
-            
-        </div>
-        
-        <div class="col-md-4">
-            <div class="card mb-3">
-
-                <h3 class="card-header">Summary</h3>
-                <div class="card-body">
-
-                </div>
-
+            <div class="myBorder">
+                <h1>Extra information</h1>
             </div>
         </div>
-    </div>
-    @endsection
+        <div class="col-md-5">
+            <div class="myBorder ">
+                <h1>Items Container</h1>
+            </div>
+        </div>
+    </div> --}}
+{{-- <div>
+        <div class="list-group" id="myTab" role="tablist">
+            <a class="list-group-item list-group-item-action active" data-bs-toggle="list" href="#home"
+                role="tab">Home</a>
+            <a class="list-group-item list-group-item-action" data-bs-toggle="list" href="#profile"
+                role="tab">Profile</a>
+            <a class="list-group-item list-group-item-action" data-bs-toggle="list" href="#messages"
+                role="tab">Messages</a>
+            <a class="list-group-item list-group-item-action" data-bs-toggle="list" href="#settings"
+                role="tab">Settings</a>
+        </div>
+
+        <div class="tab-content">
+            <div class="tab-pane active" id="home" role="tabpanel">...</div>
+            <div class="tab-pane" id="profile" role="tabpanel">...</div>
+            <div class="tab-pane" id="messages" role="tabpanel">...</div>
+            <div class="tab-pane" id="settings" role="tabpanel">...</div>
+        </div>
+
+        <script>
+            var firstTabEl = document.querySelector('#myTab a:last-child')
+            var firstTab = new bootstrap.Tab(firstTabEl)
+
+            firstTab.show()
+
+        </script>
+    </div> --}}
+{{-- <div id="menu">
+        <div class="panel list-group">
+            <a href="#" class="list-group-item" data-bs-toggle="collapse" data-bs-target="#roomsConateiner"
+                data-bs-parent="#menu">Select Rooms
+                <i class="bi bi-house-fill"></i>
+                <span class="glyphicon glyphicon-envelope pull-right"></span></a>
+            <div id="roomsConateiner" class="sublinks collapse">
+                <a href="#" class="list-group-item small"><span class="glyphicon glyphicon-chevron-right"></span>
+                    inbox</a>
+                <a class="list-group-item small "><span class="glyphicon glyphicon-chevron-right"></span> sent</a>
+            </div>
+            <a href="#" class="list-group-item" data-bs-toggle="collapse" data-bs-target="#sl"
+                data-bs-parent="#menu">TASKS <span class="glyphicon glyphicon-tag pull-right"></span></a>
+            <div id="sl" class="sublinks collapse">
+                <a class="list-group-item small"><span class="glyphicon glyphicon-chevron-right"></span> saved tasks</a>
+                <a class="list-group-item small"><span class="glyphicon glyphicon-chevron-right"></span> add new
+                    task</a>
+            </div>
+        </div>
+    </div> --}}
+@endsection
