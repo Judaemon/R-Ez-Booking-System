a299ce6fb918d06039b220d2b73b0037354d37e0
azarashiidesu <88758080+azarashiidesu@users.noreply.github.com>
2021-10-19 15:41:41 +0800

    rental image and seeder update

--- database/seeders/RentalSeeder.php
+++ database/seeders/RentalSeeder.php
@@ -23,7 +23,7 @@ class RentalSeeder extends Seeder
                 'name' => 'ATV Solo',
                 'price' => '800.00',                            //per hour
                 'description' => 'Enjoy the thrilling and adrenaline pumping trails that we have for you.',
-                'image_path' => 'Rentalimage_path',
+                'image_path' => 'rentals/atv_rental.jpg',
                 'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
                 'updated_at' => Carbon::now()->format('Y-m-d H:i:s'),
             ],
@@ -31,7 +31,7 @@ class RentalSeeder extends Seeder
                 'name' => 'ATV With Backride',
                 'price' => '1200.00',                           //per hour
                 'description' => 'Ride with a partner over the sand and dirt.',
-                'image_path' => 'Rentalimage_path',
+                'image_path' => 'rentals/atv_rental.jpg',
                 'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
                 'updated_at' => Carbon::now()->format('Y-m-d H:i:s'),
             ],
@@ -39,7 +39,7 @@ class RentalSeeder extends Seeder
                 'name' => 'Tent',
                 'price' => '550.00',
                 'description' => 'Experience the great outdoors and relax as you hear the waves roll in front of you.',
-                'image_path' => 'Rentalimage_path',
+                'image_path' => 'rentals/tent_rental.jpg',
                 'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
                 'updated_at' => Carbon::now()->format('Y-m-d H:i:s'),
             ],
@@ -47,7 +47,7 @@ class RentalSeeder extends Seeder
                 'name' => 'Volleyball',
                 'price' => '250.00',                           //per 2 hours
                 'description' => 'Have a fun and exciting match with your friends.',
-                'image_path' => 'Rentalimage_path',
+                'image_path' => 'rentals/volleyball_rental.jpg',
                 'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
                 'updated_at' => Carbon::now()->format('Y-m-d H:i:s'),
             ],
@@ -55,7 +55,7 @@ class RentalSeeder extends Seeder
                 'name' => 'Surf',
                 'price' => '1000.00',                          //board and instructor
                 'description' => 'Ride the incredible waves of Liwliwa.',
-                'image_path' => 'Rentalimage_path',
+                'image_path' => 'rentals/surf_rental.jpg',
                 'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
                 'updated_at' => Carbon::now()->format('Y-m-d H:i:s'),
             ],
