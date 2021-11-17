3fdda4f355b6831d83bd956f073fbdd369139fbf
azarashiidesu <88758080+azarashiidesu@users.noreply.github.com>
2021-11-17 21:37:46 +0800

    seeder changed to new format

--- database/seeders/RentalSeeder.php
+++ database/seeders/RentalSeeder.php
@@ -28,43 +28,42 @@ class RentalSeeder extends Seeder
                 'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
                 'updated_at' => Carbon::now()->format('Y-m-d H:i:s'),
             ],
-            // [
-            //     'rental_type' => 'ATV With Backride',
-            //     'rental_count' => '1',
-            //     'price' => '1200.00',                           //per hour
-            //     'description' => 'Ride with a partner over the sand and dirt.',
-            //     'image_paths' => ['rentals/atv_rental.jpg'],
-            //     'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
-            //     'updated_at' => Carbon::now()->format('Y-m-d H:i:s'),
-            // ],
-            // [
-                
-            //     'rental_type' => 'Tent',
-            //     'rental_count' => '1',
-            //     'price' => '550.00',
-            //     'description' => 'Experience the great outdoors and relax as you hear the waves roll in front of you.',
-            //     'image_paths' => ['rentals/tent_rental.jpg'],
-            //     'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
-            //     'updated_at' => Carbon::now()->format('Y-m-d H:i:s'),
-            // ],
-            // [
-            //     'rental_type' => 'Volleyball',
-            //     'rental_count' => '1',
-            //     'price' => '250.00',                           //per 2 hours
-            //     'description' => 'Have a fun and exciting match with your friends.',
-            //     'image_paths' => ['rentals/volleyball_rental.jpg'],
-            //     'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
-            //     'updated_at' => Carbon::now()->format('Y-m-d H:i:s'),
-            // ],
-            // [
-            //     'rental_type' => 'Surf',
-            //     'rental_count' => '1',
-            //     'price' => '1000.00',                          //board and instructor
-            //     'description' => 'Ride the incredible waves of Liwliwa.',
-            //     'image_paths' => ['rentals/surf_rental.jpg'],
-            //     'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
-            //     'updated_at' => Carbon::now()->format('Y-m-d H:i:s'),
-            // ],
+            [
+                'rental_type' => 'ATV With Backride',
+                'rental_count' => '1',
+                'price' => '1200.00',                           //per hour
+                'description' => 'Ride with a partner over the sand and dirt.',
+                'image_paths' => json_encode(['rentals/atv_rental.jpg', 'rentals/atv_rental.jpg']),
+                'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+                'updated_at' => Carbon::now()->format('Y-m-d H:i:s'),
+            ],
+            [   
+                'rental_type' => 'Tent',
+                'rental_count' => '1',
+                'price' => '550.00',
+                'description' => 'Experience the great outdoors and relax as you hear the waves roll in front of you.',
+                'image_paths' => json_encode(['rentals/tent_rental.jpg', 'rentals/tent_rental.jpg']),
+                'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+                'updated_at' => Carbon::now()->format('Y-m-d H:i:s'),
+            ],
+            [
+                'rental_type' => 'Volleyball',
+                'rental_count' => '1',
+                'price' => '250.00',                           //per 2 hours
+                'description' => 'Have a fun and exciting match with your friends.',
+                'image_paths' => json_encode(['rentals/volleyball_rental.jpg', 'rentals/tent_rental.jpg']),
+                'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+                'updated_at' => Carbon::now()->format('Y-m-d H:i:s'),
+            ],
+            [
+                'rental_type' => 'Surf',
+                'rental_count' => '1',
+                'price' => '1000.00',                          //board and instructor
+                'description' => 'Ride the incredible waves of Liwliwa.',
+                'image_paths' => json_encode(['rentals/surf_rental.jpg', 'rentals/tent_rental.jpg']),
+                'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+                'updated_at' => Carbon::now()->format('Y-m-d H:i:s'),
+            ],
         ];
 
         Rental::insert($rentals);
