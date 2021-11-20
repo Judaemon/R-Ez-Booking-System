ddca42e8bff826ea48439790753f48e93aac6df9
Arellano Hanz Mark <69075813+bskiller211@users.noreply.github.com>
2021-11-20 17:16:54 +0800

    Rental Image Uplade vol.2

--- public/js/admin/rental.js
+++ public/js/admin/rental.js
@@ -155,68 +155,68 @@ function errorWarning() {
 // });
 
 //Add Ajax
-// $('#addRentalForm').on('submit', function (event) {
-//     event.preventDefault();
+$('#addRentalForm').on('submit', function (event) {
+    event.preventDefault();
 
-//     swalWithBootstrapButtons.fire({
-//         title: 'Are you sure?',
-//         text: "This Rental information will be added from the database!",
-//         icon: 'warning',
-//         showCancelButton: true,
-//         confirmButtonText: 'Yes, Add it!',
-//         cancelButtonText: 'No, Cancel!',
-//         reverseButtons: true
+    swalWithBootstrapButtons.fire({
+        title: 'Are you sure?',
+        text: "This Rental information will be added from the database!",
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonText: 'Yes, Add it!',
+        cancelButtonText: 'No, Cancel!',
+        reverseButtons: true
 
-//     }).then((result) => {
-//         if (result.isConfirmed) {
-//             const form = this;
+    }).then((result) => {
+        if (result.isConfirmed) {
+            const form = this;
 
-//             $.ajax({
-//                 url: $(form).attr('action'),
-//                 method: $(form).attr('method'),
-//                 type: 'POST',
-//                 dataType: 'JSON',
-//                 data: new FormData(form),
-//                 processData: false,
-//                 contentType: false,
-//                 beforeSend: function () {
-//                     clearErrorText('addRentalForm');
-//                 },
-//                 success: function (response) {
-//                     if (response.status == 0) {
-//                         $.each(response.error, function (prefix, val) {
-//                             $('#addRentalForm #input_' + prefix).addClass('is-invalid')
-//                             $('#addRentalForm span.' + prefix + '_error').text(val)
-//                         })
-//                     }
+            $.ajax({
+                url: $(form).attr('action'),
+                method: $(form).attr('method'),
+                type: 'POST',
+                dataType: 'JSON',
+                data: new FormData(form),
+                processData: false,
+                contentType: false,
+                beforeSend: function () {
+                    clearErrorText('addRentalForm');
+                },
+                success: function (response) {
+                    if (response.status == 0) {
+                        $.each(response.error, function (prefix, val) {
+                            $('#addRentalForm #input_' + prefix).addClass('is-invalid')
+                            $('#addRentalForm span.' + prefix + '_error').text(val)
+                        })
+                    }
 
-//                     if (response.status == 1) {
-//                         $('#addRentalModal').modal('hide');
-//                         getRentalTable();
-//                         $(form)[0].reset();
+                    if (response.status == 1) {
+                        $('#addRentalModal').modal('hide');
+                        getRentalTable();
+                        $(form)[0].reset();
 
-//                         swalWithBootstrapButtons.fire(
-//                             'Successful!',
-//                             response.msg,
-//                             'success'
-//                         )
-//                     }
-//                 },
-//                 error: function (response) {
-//                     errorWarning()
-//                 }
-//             });
-//         } else if (
-//             result.dismiss === Swal.DismissReason.cancel // click ayaw
-//         ) {
-//             swalWithBootstrapButtons.fire(
-//                 'Cancelled',
-//                 'Lets pretend that never happend >:)',
-//                 'error'
-//             )
-//         }
-//     })
-// });
+                        swalWithBootstrapButtons.fire(
+                            'Successful!',
+                            response.msg,
+                            'success'
+                        )
+                    }
+                },
+                error: function (response) {
+                    errorWarning()
+                }
+            });
+        } else if (
+            result.dismiss === Swal.DismissReason.cancel // click ayaw
+        ) {
+            swalWithBootstrapButtons.fire(
+                'Cancelled',
+                'Lets pretend that never happend >:)',
+                'error'
+            )
+        }
+    })
+});
 
 // Display Edit Form
 $(document).on('click', '#rentalUpdateBtn', function (event) {
