d5f51322d8955f30940d9d6e7b3fca7994389f05
Omenite <65340148+Omenite@users.noreply.github.com>
2021-11-23 18:17:40 +0800

    updateuser fix

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -115,11 +115,11 @@ class UserController extends Controller
     {
         $validated = Validator::make($request->all(), [
             'account_type' => 'required|string|max:255',
-            'email' => 'required|string|email|max:255|unique:users,email'.$user->id,
-            'contact_number' => 'required|Numeric',
-            // 'contact_number' => 'required|regex:/^[(][0-9]{2}[)][\s][0-9]{3}[-][0-9]{4}$/',
-            'firstname' => 'required|string|max:255',
-            'lastname' => 'required|string|max:255',
+            // 'email' => 'required|string|email|max:255|unique:users,email'.$user->id,
+            // 'contact_number' => 'required|Numeric',
+            // // 'contact_number' => 'required|regex:/^[(][0-9]{2}[)][\s][0-9]{3}[-][0-9]{4}$/',
+            // 'firstname' => 'required|string|max:255',
+            // 'lastname' => 'required|string|max:255',
             // 'password' => 'required|string|min:8|max:255',
 
         ]);
